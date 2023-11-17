2a68088c57ce8821f2384a4a7269d4e6cea0b4a5
Franz <106358025+Franzdomingo@users.noreply.github.com>
2023-11-17 23:46:23 +0800

    Refactor ProductApplication class to remove obsolete code

--- src/main/java/com/ctb/classes/ProductApplication.java
+++ src/main/java/com/ctb/classes/ProductApplication.java
@@ -1,32 +1,11 @@
 package com.ctb.classes;
 
-import java.util.Calendar;
 import java.util.Objects;
 import java.util.Random;
 
-public class ProductApplication { // TODO: obsolete file
-    private static final Calendar calendar = Calendar.getInstance();
+public class ProductApplication {
+
     private static final Random rand = new Random();
-    private String productID;
-    private String productType;
-
-    /*----------------------Setter Methods----------------------*/
-    public void setProductID(String productID) {
-        this.productID = productID;
-    }
-
-    public void setProductType(String productType) {
-        this.productType = productType;
-    }
-
-    /*----------------------Getter Methods----------------------*/
-    public String getProductID() {
-        return productID;
-    }
-
-    public String getProductType() {
-        return productType;
-    }
 
     /*----------------------Class Methods----------------------*/
     protected static long generateProductID(final String productType) {
